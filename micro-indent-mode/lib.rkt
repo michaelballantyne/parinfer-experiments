97628c348208f4a1ea6910b3e4acc745b893e07b
Michael Ballantyne <michael.ballantyne@gmail.com>
2016-09-25 15:48:52 -0600

    remove some duplicate cases

--- micro-indent-mode/lib.rkt
+++ micro-indent-mode/lib.rkt
@@ -4,6 +4,10 @@
 
 (struct ctx (parent column left) #:transparent)
 
+(define (ctx-push c item)
+  (struct-copy ctx c
+               [left (cons item (ctx-left c))]))
+
 (define (parinfer str)
   (reverse
    (ctx-left
@@ -16,47 +20,37 @@
     (match-define (ctx parent column left) c)
     (if (< column count)
         c
-        (close-ctx (struct-copy ctx parent
-                                [left (cons (reverse left)
-                                            (ctx-left parent))]))))
+        (close-ctx (ctx-push parent (reverse left)))))
+  
   (match s
     [(cons #\space r)
      (parse-indent r c (add1 count))]
     [(cons #\newline r)
      (parse-indent r c)]
-    [(cons ch r)   
+    [(cons ch r)
      (parse-line s (close-ctx c) count)]
     ['()
      (close-ctx c)]))
 
 (define (parse-identifier s c col [chars '()])
-  (define (id-finished-ctx)
-    (struct-copy ctx c
-                 [left (cons (string->symbol (list->string (reverse chars)))
-                             (ctx-left c))]))
+  (define (push-id)
+    (ctx-push c (string->symbol (list->string (reverse chars)))))
+  
   (match s
-    [(cons #\space r)
-     (parse-line r (id-finished-ctx) (add1 col))]
-    [(cons #\) _)
-     (parse-line s (id-finished-ctx) col)]
-    [(cons #\newline r)
-     (parse-indent r (id-finished-ctx))]
+    [(or (cons (or #\) #\space #\newline) _) '())
+     (parse-line s (push-id) col)]
     [(cons ch r)
-     (parse-identifier r c (add1 col) (cons ch chars))]
-    ['()
-     (parse-indent '() (id-finished-ctx))]))
+     (parse-identifier r c (add1 col) (cons ch chars))]))
 
 (define (parse-line s c col)
   (match s
     [(cons #\( r)
      (parse-line r (ctx c col '()) (add1 col))]
-    [(cons #\newline r)
-     (parse-indent r c)]
+    [(or (cons #\newline _) '())
+     (parse-indent s c)]
     [(cons #\) r)
-     (match-define (ctx parent _ left) c)
-     (parse-line r (struct-copy ctx parent [left (cons (reverse left) (ctx-left parent))]) (add1 col))]
-    ['()
-     (parse-indent '() c)]
+     (match-let ([(ctx parent _ left) c])
+       (parse-line r (ctx-push parent (reverse left)) (add1 col)))]
     [(cons #\space r)
      (parse-line r c (add1 col))]
     [_
