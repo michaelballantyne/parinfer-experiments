3d8e22824c276dbf3cef77e52d58dafd6a1611eb
Michael Ballantyne <michael.ballantyne@gmail.com>
2016-09-27 23:20:12 -0600

    use the at-exp reader to make the tests nicer

--- micro-indent-mode/test.rkt
+++ micro-indent-mode/test.rkt
@@ -1,88 +1,87 @@
-#lang racket
+#lang at-exp racket
 
 (require "with-lexer.rkt"
          rackunit)
 
 (check-equal?
- '((a) b)
- (parinfer "
-(a
-b
-"))
+ (parinfer
+  @string-append{
+    (a
+    b
+  })
+ '((a) b))
 
 (check-equal?
- '((a b))
- (parinfer "
-(a
- b
-"))
+ (parinfer
+  @string-append{
+    (a
+     b
+  })
+ '((a b)))
 
 (check-equal?
- '((a b))
- (parinfer "
-(a
- b
-"))
+ (parinfer
+  @string-append{
+   (a
+    b
+
+  })
+ '((a b)))
 
 (check-equal?
- '((a b))
- (parinfer "
-(a
- b
-"))
+ (parinfer
+  @string-append{
+    
+    (a
+     b
+  })
+ '((a b)))
 
 (check-equal?
- '((a (b) c))
- (parinfer "
-(a (b
- c
-"))
+ (parinfer
+  @string-append{
+    (a (b
+     c
+  })
+ '((a (b) c)))
 
 (check-equal?
- '((a (b) c))
- (parinfer "
-(a (b
- c
-"))
+ (parinfer
+  @string-append{
+   (a (b
+      c
+  })
+ '((a (b) c)))
 
 (check-equal?
- '((a (b) c))
- (parinfer "
-(a (b
-   c
-"))
+ (parinfer
+  @string-append{
+    (a (b
+        c
+  })
+ '((a (b c))))
 
 (check-equal?
- '((a (b c)))
- (parinfer "
-(a (b
-    c
-"))
+ (parinfer
+  @string-append{
+    (a (b
+       c)
+  })
+ '((a (b) c)))
 
 (check-equal?
- '((a (b) c))
- (parinfer "
-(a (b
-   c"))
-
-(check-equal?
- '((a (b) c))
- (parinfer "
-(a (b
-   c)"))
-
-(check-equal?
+ (parinfer
+  @string-append{
+    (define (append l s
+      (cond
+        ((null? l
+         s
+        (else
+         (cons (car l) (append (cdr l) s
+  })
  '((define (append l s)
      (cond
        ((null? l)
         s)
        (else
-        (cons (car l) (append (cdr l) s))))))
- (parinfer "
-(define (append l s
-  (cond
-    ((null? l
-     s
-    (else
-     (cons (car l) (append (cdr l) s
-"))
+        (cons (car l) (append (cdr l) s)))))))
